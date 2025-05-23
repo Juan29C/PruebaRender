4400ec47ff4d34c9f12ae0729d1cdc2868b8ed85
DaniSScript <carrascosantosdanielbenjamin@gmail.com>
2025-05-23 15:18:00 -0500

    Subiendo Repositorio

---
+++ src/main/java/com/mdnch/webmdnch/WebmdnchApplication.java
@@ -0,0 +1,14 @@
+package com.mdnch.webmdnch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmdnchApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Starting WebMDNCH Application...");
+		SpringApplication.run(WebmdnchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdnch/webmdnch/WebmdnchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdnch.webmdnch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmdnchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
