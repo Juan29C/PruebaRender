db9502aaff61cf8176e731ad5235b2749539fb5a
Juan29C <juan.luk96@gmail.com>
2025-06-17 15:15:09 -0500

    Cambio pequeño

--- src/main/java/com/mdnch/webmdnch/dto/request/NoticiasFormRequest.java
+++ src/main/java/com/mdnch/webmdnch/dto/request/NoticiasFormRequest.java
@@ -6,12 +6,41 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
-@Data
-@Getter
-@Setter
 public class NoticiasFormRequest {
     private String titulo;
     private String categoria;
     private String descripcion;
     private MultipartFile imagen;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public MultipartFile getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(MultipartFile imagen) {
+        this.imagen = imagen;
+    }
 }
